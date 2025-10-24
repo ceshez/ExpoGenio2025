c993ccb5f4d931ee8238e9d651dc40e02acbfbab
Yerick <Yerickmondragonsancho@gmail.com>
2025-10-24 16:12:00 -0600

    blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,11 @@
-import VideoSection from "./InfoVideo"
+import Blog from "./blog"
 import Header from "../components/layout/Header"
 import Footer from "../components/layout/Footer"
 export default function Page() {
   return (
     <main>
         <Header/>
-      <VideoSection />
+      <Blog />
         <Footer/>
 
     </main>
