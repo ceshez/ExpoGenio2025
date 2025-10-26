fe17854650887c0664d3941f1e75c5e2a52f4b56
ceshez <carlossanchezher10@gmail.com>
2025-10-25 19:29:04 -0600

    arreglar rutas
    rutas de elementos arregladas

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import Blog from "./blog"
-import Header from "../components/layout/Header"
-import Footer from "../components/layout/Footer"
+import Header from "../components/Layout/Header"
+import Footer from "../components/Layout/Footer"
 export default function Page() {
   return (
     <main>
