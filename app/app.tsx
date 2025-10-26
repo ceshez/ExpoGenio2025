1ab75ce63fa22e299b58cc9b7b26792cf1f8ede4
XiaoMendez <xiaomendeli@gmail.com>
2025-10-25 20:51:04 -0600

    Router

--- app/app.tsx
+++ app/app.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Importar componentes comunes
 import LoadingScreen from './components/Common/LoadingScreen';
