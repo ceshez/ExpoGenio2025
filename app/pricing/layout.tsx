6145c8abb68e611b5dc671f967d616e0fcd2c00e
yerick <yerickmondragonsancho@gmail.com>
2025-11-11 09:52:06 -0600

    update pricing page

--- app/pricing/layout.tsx
+++ app/pricing/layout.tsx
@@ -12,23 +12,6 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Seccion Precios",
   description: "Sección de Precios",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
 }
 
 export default function RootLayout({
