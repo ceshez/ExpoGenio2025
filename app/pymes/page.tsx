ca0d11531d0d2514ae88337e8b355e7f96c37238
yerick <yerickmondragonsancho@gmail.com>
2025-11-11 23:55:55 -0600

    fix: added default export in pymes page

---
+++ app/pymes/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold text-white">Página de PYMES</h1>
+    </main>
+  )
+}
